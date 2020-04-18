ce6194377c147633483c79ba44187f6ec6f7ce21
Luming Hao <lh00000000@gmail.com>
2020-04-17 21:24:31 -0400

    some quitting tactics

--- wertheimer.py
+++ wertheimer.py
@@ -1,6 +1,7 @@
 import mido
 import requests
 import signal
+import os
 import random
 import itertools
 import time
@@ -10,43 +11,61 @@ import asyncio
 
 import record
 import midi
+
 # import jobs
 import opuses
 from dotenv import load_dotenv
+
 load_dotenv()
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
 
 
 async def record_job(opus):
-    wav_filename = "-".join([
-            # datetime.datetime.utcnow().isoformat()[0:10],
-            # config.KITTY_NAME,
-            str(opus["_id"]),
-        ]) + ".wav"
+    wav_filename = (
+        "-".join(
+            [
+                # datetime.datetime.utcnow().isoformat()[0:10],
+                # config.KITTY_NAME,
+                str(opus["_id"])
+            ]
+        )
+        + ".wav"
+    )
 
     new_recording = record.LoserRecording(wav_filename)
     new_recording.record()
     await midi.output_midi(opus["events"])
     await new_recording.schedule_stop()
 
+
+should_loop = True
+
+
 def signal_handler(signal, frame):
-    loop.stop()
-    client.close()
-    sys.exit(0)
+    global should_loop
+    logging.info("signit received. ending.")
+    should_loop = False
 
 
 signal.signal(signal.SIGINT, signal_handler)
 
+
 def main():
-    print("starting")
-    while True:
+    global should_loop
+    logging.debug(f"starting wertheimer-performer={os.getenv('PERFORMER_NAME')}")
+
+    while should_loop:
         assigned_opus = opuses.get_assigned()
         if assigned_opus:
             asyncio.run(record_job(assigned_opus))
             opuses.upload_opus(assigned_opus)
         else:
-            time.sleep(3)
+            logging.debug(f"sleeping for POLL_PERIOD={os.getenv("POLL_PERIOD")}")
+            time.sleep(int(os.getenv("POLL_PERIOD")))
+
+
 
 if __name__ == "__main__":
     main()
